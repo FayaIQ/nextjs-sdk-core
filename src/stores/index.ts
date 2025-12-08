e717d6d6fd838851318682894aa8d3c01bc3c800
zhraa2605 <zhraa.e.l.2605@gmail.com>
2025-12-08 09:59:13 +0300

    feat: Re-export getStoreDeliveryZones in stores index

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,5 +4,6 @@ export type { Store } from "./store-models";
 // Re-export handler for Next.js API route wiring
 export { GET as GETStores } from "./handler/getStores";
 export * from "./getStoreUsersPaging";
+export * from "./getStoreDeliveryZones";
 export type { StoreUser, StoreUsersPagingResponse } from "./store-users-models";
 export { GET as GETStoreUsersPaging } from "./handler/getStoreUsersPaging";
