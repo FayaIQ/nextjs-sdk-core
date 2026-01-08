ac1d9100d1f1fef89902589ebadbba4c57fcd43d
ahmedsa04 <ahmedalshadad5@gmail.com>
2026-01-08 07:47:10 +0300

    V1.0

--- dist/getOffersDeliveryZones-ZR5PGM7G.js
+++
@@ -1,6 +0,0 @@
-import {
-  getOffersDeliveryZones
-} from "./chunk-47A2JI5U.js";
-export {
-  getOffersDeliveryZones
-};
--- dist/token-ZNWRIMOQ.js
+++
@@ -1,6 +0,0 @@
-import {
-  getToken
-} from "./chunk-HALIJFAD.js";
-export {
-  getToken as default
-};
