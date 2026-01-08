58299fa4a11b9b218695e77fa77a9c3485cf6663
zhraa2605 <zhraa.e.l.2605@gmail.com>
2026-01-08 15:17:03 +0300

    refactor: remove comment for crypto utilities in index.ts

--- src/index.ts
+++ src/index.ts
@@ -47,6 +47,5 @@ export {
   SECURE_COOKIE_OPTIONS,
 } from "./utils/cookie";
 
-// Crypto utilities (server-side encryption/decryption)
 export { encrypt, decrypt, encryptSync, decryptSync } from "./utils/crypto";
 
