6ff105d11b3679e3cfea73b79782fafb42e9b6f1
Yousif Khalid <yousifk55@gmail.com>
2026-07-28 18:29:27 +0300

    Use export-star barrels for DTS compatibility

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,23 +1,2 @@
-export {
-  getWithAuth,
-  getWithoutAuth,
-  postWithAuth,
-  postWithoutAuth,
-  putWithAuth,
-  putWithoutAuth,
-  deleteWithAuth,
-  deleteWithoutAuth,
-  patchWithAuth,
-  patchWithoutAuth,
-  ApiRequestOptions,
-  RequestData,
-  QueryParams,
-  Primitive,
-} from "./fetcher";
-
-export {
-  getAuthConfig,
-  getCookieTTLConfig,
-  AuthConfig,
-  CookieTTLConfig,
-} from "./config";
+export * from "./fetcher";
+export * from "./config";
